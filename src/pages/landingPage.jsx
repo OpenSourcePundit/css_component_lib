8470490832f639be56fcbbd840688df4ee402092
Shashank Kumar <shashank00098@gmail.com>
2023-11-23 18:01:00 +0530

    fix

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom'
-
-import Footer from "./components/footer";
 const LandingPage = () => {
     const navigate = useNavigate();
   return (
