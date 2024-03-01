d8b3727674203e60e49ab59c5b11acb14742355a
Shashank Kumar <shashank00098@gmail.com>
2024-03-02 00:47:23 +0530

    fix: Github External link fixed.

--- src/pages/components/navbar.jsx
+++ src/pages/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
+
 const NavBar = () => {
 
     const navigate = useNavigate();
@@ -17,7 +18,7 @@ const NavBar = () => {
             <div className="a" onClick={()=>navigate('./components')}>All Components</div>
           </li>
           <li>
-            <div className="a" onClick={()=>navigate('https://github.com/OpenSourcePundit/css_component_lib')}>Docs-GitHub</div>
+            <Link style={{textDecoration:"none"}} className="a" to='https://github.com/OpenSourcePundit/css_component_lib'  target="_blank" >Docs-GitHub</Link>
           </li>
         </ul>
       </div>
